a644396df3011cd85f6948fde0efc8ba60b4b670
Julian <julian.ronge@live.de>
2016-09-08 08:57:34 +0200

    fix missing ending slash

--- MyDevoxx/Services/RestService.cs
+++ MyDevoxx/Services/RestService.cs
@@ -53,7 +53,7 @@ namespace MyDevoxx.Services
             string endpoint = (string)settings.Values["cfpEndpoint"];
             string confId = (string)settings.Values["conferenceId"];
             string country = (string)settings.Values["country"];
-            string url = endpoint + "/conferences" + "/" + confId + scheduleUrl + "/" + day;
+            string url = endpoint + "/conferences" + "/" + confId + scheduleUrl + "/" + day + "/";
             Schedule schedule = await fetch<Schedule>(url, "schedule_" + day + "_" + confId + ".json");
 
             return EventConverter.apply(schedule, confId, country);
